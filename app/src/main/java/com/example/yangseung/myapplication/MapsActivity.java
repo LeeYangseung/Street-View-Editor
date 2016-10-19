2564607bf6d182fb9033c28bce864e9159d184ca
dutkddnjs <ysw1317@gmail.com>
2016-10-19 17:48:52 +0900

    2016-10-19(17:46)

--- app/src/main/java/com/example/yangseung/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/yangseung/myapplication/MapsActivity.java
@@ -23,6 +23,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         System.out.println("Hello World!!");
+        System.out.println("Hello World!!");
     }
 
 
