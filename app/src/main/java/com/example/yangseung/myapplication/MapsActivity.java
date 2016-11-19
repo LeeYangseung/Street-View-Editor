c3eb93f381f6f90020f5d1d55f9edba5742d9e68
dutkddnjs <ysw1317@gmail.com>
2016-11-19 18:29:38 +0900

    최신버전 11-19-18:30

--- app/src/main/java/com/example/yangseung/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/yangseung/myapplication/MapsActivity.java
@@ -22,8 +22,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        System.out.println("Hello World!!");
-        System.out.println("Hello World!!");
+
     }
 
 
